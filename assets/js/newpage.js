253d77a078f050240bc9d30ea95fd9b36038be69
jobsonjuniorr <160482109+jobsonjuniorr@users.noreply.github.com>
2025-01-16 17:15:00 -0300

    Style: applied to layout forms

--- assets/js/newpage.js
+++ assets/js/newpage.js
@@ -49,3 +49,14 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
